af65c3e9a8929edd03c7c8f4db88b3fe257b66ea
NamLT <namluong220101@gmail.com>
2023-06-05 21:36:15 +0700

    add base API

--- src/redux-saga/sagas.ts
+++ src/redux-saga/sagas.ts
@@ -1,5 +1,6 @@
 import { all } from "redux-saga/effects"
+import { login, register } from "./sagas/Authentication"
 
 export default function* rootSaga() {
-  yield all([])
+  yield all([login(), register()/*, continue(), */])
 }
